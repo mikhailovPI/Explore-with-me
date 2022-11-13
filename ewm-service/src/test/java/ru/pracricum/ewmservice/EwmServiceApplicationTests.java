e3698a77daba733fbcfa9ee66d3e415890481929
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2022-11-13 22:17:10 +0300

    Develop: update tests

--- ewm-service/src/test/java/ru/pracricum/ewmservice/EwmServiceApplicationTests.java
+++ ewm-service/src/test/java/ru/pracricum/ewmservice/EwmServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package ru.pracricum.ewmservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EwmServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
